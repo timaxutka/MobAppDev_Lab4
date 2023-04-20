1e2856b1a346bc22a78050eb847c26b766a0b242
eleve <elevenfive1@gmail.com>
2023-04-20 23:07:55 +0600

    Added Activity2

---
+++ app/src/main/java/com/example/mobappdev_lab4/Activity2.kt
@@ -0,0 +1,11 @@
+package com.example.mobappdev_lab4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
