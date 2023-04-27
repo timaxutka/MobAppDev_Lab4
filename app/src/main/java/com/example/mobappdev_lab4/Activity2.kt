2c73e2ef2971b2ee8962c8c94fa729540bcbfd78
eleve <elevenfive1@gmail.com>
2023-04-27 21:02:05 +0600

    Designed the interface of MainActivity and Activity2: added buttons, added text to string.xml, added datePicker to Activity2

--- app/src/main/java/com/example/mobappdev_lab4/Activity2.kt
+++ app/src/main/java/com/example/mobappdev_lab4/Activity2.kt
@@ -1,11 +1,21 @@
 package com.example.mobappdev_lab4
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import java.util.Calendar
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        val name = intent.getStringExtra("name")
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Привет, $name!"
     }
 }
--- app/src/main/java/com/example/mobappdev_lab4/MainActivity.kt
+++ app/src/main/java/com/example/mobappdev_lab4/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.mobappdev_lab4
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.EditText
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, Activity2::class.java)
+            val name = findViewById<EditText>(R.id.editText).text.toString()
+            intent.putExtra("name", name)
+            startActivity(intent)
+        }
     }
 }
